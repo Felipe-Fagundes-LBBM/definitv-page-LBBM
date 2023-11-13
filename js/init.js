00e887343d511609a3672a8e4e287d3b7d193d42
Felipe Fagundes <felipelopesfagundes@gmail.com>
2023-11-13 16:53:35 -0300

    git add.

--- js/init.js
+++ js/init.js
@@ -31,3 +31,38 @@ function prevSlide() {
 }
 
 // fim do carroseeeeel
+
+const slider = document.querySelectorAll('.slider')
+const btnPrev = document.getElementById('prev-button');
+const btnNext = document.getElementById('next-button');
+
+let currentSlide = 0;
+
+function hideSlider () {
+  slider.forEach(item => item.classList.remove('on'));
+}
+
+function showSlider() {
+  slider[currentSlide].classList.add('on')
+}
+
+function prevNextSlider(direction) {
+  hideSlider();
+  if (direction === 'next') {
+    if (currentSlide === slider.length - 1) {
+      currentSlide = 0;
+    } else {
+      currentSlide++;
+    }
+  } else if (direction === 'prev') {
+    if (currentSlide === 0) {
+      currentSlide = slider.length - 1;
+    } else {
+      currentSlide--;
+    }
+  }
+  showSlider();
+}
+
+btnNext.addEventListener('click', () => prevNextSlider('next'));
+btnPrev.addEventListener('click', () => prevNextSlider('prev'));
