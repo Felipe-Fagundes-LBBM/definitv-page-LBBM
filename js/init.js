33eec637dbf5556988a2c9783448ad13dea4e479
Felipe Fagundes <felipelopesfagundes@gmail.com>
2023-11-16 15:20:40 -0300

    carroseu

--- js/init.js
+++ js/init.js
@@ -68,3 +68,49 @@ btnNext.addEventListener('click', () => prevNextSlider('next'));
 btnPrev.addEventListener('click', () => prevNextSlider('prev'));
 
 
+let list = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let dots = document.querySelectorAll('.selector .dots li');
+let prev = document.getElementById('.prev');
+let next = document.getElementById('.next');
+
+let active = 0;
+let lengthItems = items.length - 1;
+
+next.onclick = function(){
+  if(active + 1 > lengthItems){
+   active = 0; 
+  } else{
+    active = active + 1;
+  } 
+  reloadSlider();
+}
+
+prev.onclick = function () {
+  if(active -1 < 0){
+    active = lengthItems;
+    } else{
+      active = active - 1;
+    }
+    reloadSlider();
+}
+
+let refreshSlider = setInterval (() => {next.click()}, 3000);
+function reloadSlider (){
+  let checkLeft = items [active].offsetLeft;
+  list.style.left = -checkLeft+'px';
+
+  let lastActiveDot = document.querySelector('.slider .dots li.active');
+  lastActiveDot.classList.remove('active');
+  dots[active].classList.add('active');
+  clearInterval(refreshSlider);
+  refreshSlider = setInterval(()=> {next.click()}, 5000 )
+
+}
+
+dots.forEach((Li, key) => {
+  Li.addEventListener('click', function(){
+    active = key;
+    reloadSlider();
+  })
+})
